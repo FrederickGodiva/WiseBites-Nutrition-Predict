269ace9c28f85311d40311baebc30437aeed1ce6
FrederickGodiva <godivafrederick@gmail.com>
2024-11-13 11:47:54 +0700

    chore: initialize nextjs project with typescript, tailwindcss, and daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 export default {
   content: [
@@ -8,11 +9,8 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      colors: {},
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 } satisfies Config;
